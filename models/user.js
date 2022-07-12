c7c7687dae5e4f8a15ce5232a3c87e0f2782ba8d
Pedro Abreu <henriqueibtou@gmail.com>
2022-07-12 11:36:27 -0700

    add more planning ERD to the readme file

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const { Schema, model } = mongoose
+
+// make a user schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
