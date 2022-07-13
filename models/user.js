59d589fce0e522005afd7128603775d2174f12aa
Pedro Abreu <henriqueibtou@gmail.com>
2022-07-13 08:50:41 -0700

    added readme

--- models/user.js
+++ models/user.js
@@ -3,12 +3,12 @@ const { Schema, model } = mongoose
 // make a user schema
 const userSchema = new Schema({
     username: {
-        type: String,
+        type: String, //string come from the user name field
         required: true,
         unique: true
     },
     password: {
-        type: String,
+        type: String, //single User
         required: true
     }
 })
