6b18906521e4e230a8a50d2f852fb57015a3a415
Pedro Abreu <henriqueibtou@gmail.com>
2022-07-13 14:53:26 -0700

    Create new place , redirecting to first page

--- controller/place_routes.js
+++ controller/place_routes.js
@@ -4,6 +4,56 @@ const router = express.Router()
 //edit insert or delete
 const Place = require('../models/place')
 
+
+
+//////////////////////////
+/////////////////////////
+///////////////////////////////////
+
+
+
+
+
+
+
+//CREATE
+//GET route for displaying my form for create
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+//POST -CREATE
+router.post('/', (req,res) =>{
+    ////// basically here is a boolean ... 
+    req.body.free = req.body.free === 'on' //if free is checked...
+    ? true : false //turn on or false
+    //////
+
+
+    Place.create(req.body)
+        .then(place =>{
+            console.log(place)
+            //res.json(place) //here just display it as the begin show the item inside the array
+            res.redirect('/places') // as soon as you finish to create the new post it reload to the first page
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+
+
+
+
+
+
+
+//////////////////////////
+/////////////////////////
+///////////////////////////////////
+
+
+
 //GET - index it will show your places
 // localhost:3000/places
 router.get('/', (req,res) => {
@@ -20,7 +70,8 @@ router.get('/', (req,res) => {
     
 })
 
-//get - show
+//SHOW
+//GET - show
 //localhost:3000/places/:id ..... change with the id being passed in
 router.get('/:id', (req, res) => {
     const placeId = req.params.id
@@ -28,24 +79,27 @@ router.get('/:id', (req, res) => {
     Place.findById(placeId)
     //send back some json
     .then(place => {
-        res.json(place)
+        //res.json(place)
+        res.render('places/show', { place})
     })
     .catch(err => {
         res.json(err)
     })
 })
 
-
+//////////////////////////
+/////////////////////////
+////////////////////////////////////
 
 
 //seed route
 //insert many items
 router.get('/seed', (req,res) => {
     const startPlaces= [
-        {name: "Golden Gate",like: 0 ,address:"San Francisco" , free: true},
-        {name: "Bay Bridge",like: 0 , address:"San Francisco" , free: true},
-        {name: "Pier 39",like: 0 , address:"San Francisco" , free: true},
-        {name: "Alcatraz",like: 0 , address:"San Francisco" , free: false},
+        {name: "Golden Gate",like: 2 ,address:"San Francisco" , free: true},
+        {name: "Bay Bridge",like: 3 , address:"San Francisco" , free: true},
+        {name: "Pier 39",like: 1 , address:"San Francisco" , free: true},
+        {name: "Alcatraz",like: 6 , address:"San Francisco" , free: false},
 
     ]
 
