a45d1d498a0abe5ad10408a54746450a03821cdd
Pedro Abreu <henriqueibtou@gmail.com>
2022-07-16 21:08:32 -0700

    Added background image

---
+++ controller/upload-routes.js
@@ -0,0 +1,11 @@
+// const express = require('express')
+// const multer = require('multer')
+// //make router
+// const router = express.Router()
+// //edit insert or delete
+// const Place = require('../models/place')
+
+
+// const storage = multer.diskStorage({
+   
+// })
