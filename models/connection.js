9ae25e1aad8c82afe4e05dba19dfe0efc81f954f
Pedro Abreu <henriqueibtou@gmail.com>
2022-07-20 08:14:06 -0700

    Here added some MONGODB now it is working and running the localhost 3000 again connected to the MONGODB

--- models/connection.js
+++ models/connection.js
@@ -4,26 +4,19 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 
-const DATABASE_URI = process.env.DATABASE_URI
-const config = {
+// Fire off the connection to Mongo DB
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}
-
-// connecting our mongoDB to mongoose
-mongoose.connect(DATABASE_URI, config)
-
-mongoose.connection
-// handle the opening of the connections
-// running code block on open
-// console.logging a string
-    .on('open', () => console.log('Connected to Mongoose'))
-    // since we have opened a connection we've got to close it
-    // running a code block on close
-    .on('close', () => console.log('Disconnected from Mongoose'))
-    // handle any error that might happen
-    // running a code block on error
-    // using console.error to see that error
-    .on('error', err => console.error(err))
+  });
+  
+  
+  mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`);
+  });
+  
+  mongoose.connection.on("error", (err) => {
+    console.log("Could not connect to MongoDB!", err);
+  });
 
 module.exports = mongoose
